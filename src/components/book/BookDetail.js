5b3643b467f133b690bd118ad878c9af280ebadd
Allie Zhao <allie.k.zhao@gmail.com>
2023-01-23 16:26:42 -0800

    didn't commit at all before this but I'm done now

--- src/components/book/BookDetail.js
+++ src/components/book/BookDetail.js
@@ -1,20 +1,27 @@
 import Book from './Book';
 import { useBookDetail } from '../../hooks/useBookDetail';
+import { useParams, Link } from 'react-router-dom';
 
 function BookDetail() {
-  const id = 1; // TODO: Use id from route
+  const id = useParams();
   const { book, loading, error } = useBookDetail(id);
 
   if (error)
     return (
       <>
         <h1>Something went wrong :(</h1> <h2>{error}</h2>
+        <Link to="/catalog">Go to catalog?</Link>
       </>
     );
 
   if (loading) return <h3>Loading book...</h3>;
 
-  return <Book book={book} showDetail />;
+  return (
+    <>
+      <Book book={book} showDetail />
+      <Link to="/catalog">Back to Catalog</Link>
+    </>
+  );
 }
 
 export default BookDetail;
